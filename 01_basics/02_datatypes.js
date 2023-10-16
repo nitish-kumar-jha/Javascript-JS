0f45931010248c87ceed11a69c5059f5b904c01f
Nitish Kumar Jha <147186720+nitish-kumar-jha@users.noreply.github.com>
2023-10-16 17:02:35 +0000

    data types intro

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //for treating all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log("Nitish")
+
+let name = "nitish"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+//number => 2to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
