bdf5ae01bb55899b744248399857fc983542a45d
Nitish Kumar Jha <147186720+nitish-kumar-jha@users.noreply.github.com>
2023-10-16 16:17:49 +0000

    Learned basics of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 45789;
+let accountEmail = "nitish@amazon.com";
+var accountPassword = "12345";
+accountCity = "Chapra";
+
+// accountId = 8447;
+accountEmail="nskcnc@gmail.com"
+accountCity = "Delhi";
+console.log(accountEmail);
+
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+/*
+Prefer always to use let instead of var.
+*/ 
