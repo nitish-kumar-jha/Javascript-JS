36e0693d5d0d54dda413dae9086020b816334500
Nitish kumar jha <jhanitish1312@gmail.com>
2023-10-27 20:15:33 +0530

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(3>8);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);       //Note :- equal to operator or comparison operator not work same in javascript
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined<0);
+
+
+// ===
+console.log("2" === 2);
