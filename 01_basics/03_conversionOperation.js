3f55df5d167204c010d15ce4c16e4011781f26f4
Nitish kumar jha <jhanitish1312@gmail.com>
2023-10-27 17:07:31 +0530

    changes added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,49 @@
-let score = "nitish"
-console.log(score)
-console.log(typeof score)
+let score = "hitesh"
+
+// console.log(score)
+// console.log(typeof score)
+
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn = "nitish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+//" " => false
+//"hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  *********************operations********
+
+let value =3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**5)
+// console.log(2/5)
+// console.log( Math.floor(25/5))
+// console.log(2%3);
+
+let str1  = "hello"
+let str2 = "nitish"
+
+
+
